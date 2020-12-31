b44a80a2eb958e7859e68bb4111d9f147548df0a
chochoq <aubepe@gmail.com>
2020-12-31 23:41:52 +0900

    1231

--- intelliJ-workspace/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ intelliJ-workspace/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,10 +15,41 @@ public class HelloController {
         return "hello";
     }
 
-    @GetMapping("hello-mvc")
+    @GetMapping("hello-mvc")//http://localhost:8080/hello-mvc?name=spring
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name",name);
         return "hello-template";
 
     }
+
+    @GetMapping("hello-string") //http://localhost:8080/hello-string?name=spring
+    @ResponseBody
+    public String hellSpring(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    //api 방식 json방식
+    @GetMapping("hello-api") //http://localhost:8080/hello-api?name=spring
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+
+    //api를 위한 객체(레디겟셋 > javabean 표준, 프로퍼티접근방식
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
 }
