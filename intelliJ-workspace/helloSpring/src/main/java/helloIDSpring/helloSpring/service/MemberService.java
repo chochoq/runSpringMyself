54c62139d5d39d4bd217e6294438269dbe63bb59
chochoq <aubepe@gmail.com>
2021-01-06 17:30:43 +0900

    member 서비스

---
+++ intelliJ-workspace/helloSpring/src/main/java/helloIDSpring/helloSpring/service/MemberService.java
@@ -0,0 +1,39 @@
+package helloIDSpring.helloSpring.service;
+
+import helloIDSpring.helloSpring.domain.Member;
+import helloIDSpring.helloSpring.repository.MemberRepository;
+import helloIDSpring.helloSpring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //회원가입
+    public Long join(Member member) {
+        //중복회원 x
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    //중복회원 검증
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다");
+                });
+    }
+
+    //모든 회원조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
