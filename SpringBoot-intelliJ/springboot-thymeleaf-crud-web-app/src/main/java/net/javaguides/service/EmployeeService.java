c0f69a625c07c4354d9d4d0cd30e9ee5ae4f6d97
chochoq <aubepe@gmail.com>
2021-01-22 16:41:12 +0900

    Implement List Employee Feature

---
+++ SpringBoot-intelliJ/springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package net.javaguides.service;
+
+import net.javaguides.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployee();
+
+}
---
+++ SpringBoot-intelliJ/springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/service/EmployeeServiceImpl.java
@@ -0,0 +1,23 @@
+package net.javaguides.service;
+
+import net.javaguides.model.Employee;
+import net.javaguides.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployee() {
+        return employeeRepository.findAll();
+    }
+
+
+}
