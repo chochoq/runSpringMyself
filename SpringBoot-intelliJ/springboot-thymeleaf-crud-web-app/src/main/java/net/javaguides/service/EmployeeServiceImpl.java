af6641adb22f4bedae8c273de042a8c33b0bb7ff
chochoq <aubepe@gmail.com>
2021-01-24 14:40:56 +0900

    Implement List Employee style

--- SpringBoot-intelliJ/springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/service/EmployeeServiceImpl.java
+++ SpringBoot-intelliJ/springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/service/EmployeeServiceImpl.java
@@ -19,5 +19,10 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employeeRepository.findAll();
     }
 
+    @Override
+    public void saveEmployee(Employee employee) {
+        this.employeeRepository.save(employee);
+    }
+
 
 }
