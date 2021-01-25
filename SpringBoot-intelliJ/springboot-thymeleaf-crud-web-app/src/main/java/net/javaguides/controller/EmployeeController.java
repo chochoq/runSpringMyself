8abdf57000a43bf015d1c523f0c5b5c72ed3e690
chochoq <aubepe@gmail.com>
2021-01-25 18:40:06 +0900

    Implement End-to-End Add Employee Feature

--- SpringBoot-intelliJ/springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/controller/EmployeeController.java
+++ SpringBoot-intelliJ/springboot-thymeleaf-crud-web-app/src/main/java/net/javaguides/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -17,5 +19,20 @@ public class EmployeeController {
     public String viewHomePage(Model model) {
         model.addAttribute("listEmployees", employeeService.getAllEmployee());
         return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model) {
+        //create model attribute to bind form data
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        //save Employee to db
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
     }
 }
