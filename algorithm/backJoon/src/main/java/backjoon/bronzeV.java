38d14302b0d7a0b4dfbcf80ec42b6aaa8a12cdd1
chochoq <aubepe@gmail.com>
2021-02-02 15:53:02 +0900

    (feat) add 1550 solve

--- algorithm/backJoon/src/main/java/backjoon/bronzeV.java
+++ algorithm/backJoon/src/main/java/backjoon/bronzeV.java
@@ -1,5 +1,7 @@
 package backjoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class bronzeV {
@@ -7,18 +9,21 @@ public class bronzeV {
 //    https://solved.ac/problems/level
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 //    1000 A+B 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 //    1001 A-B 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
         Scanner sc = new Scanner(System.in);
+        System.out.println("A+B,A-B");
         int a, b;
         a = sc.nextInt();
         b = sc.nextInt();
 
         System.out.println(a + b);
         System.out.println(a - b);
+        System.out.println("");
 
 //    1271 엄청난 부자2
+        System.out.println("1271 엄청난 부자2");
         int m;
         int n;
         m = sc.nextInt();
@@ -28,7 +33,13 @@ public class bronzeV {
         System.out.println(m % n);
 
 //   1550 16진수
+        System.out.println("1550 16진수");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(Integer.parseInt(input, 16));
 
 
     }
+
 }
