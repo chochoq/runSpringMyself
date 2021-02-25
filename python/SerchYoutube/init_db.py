810b94c3e23c0ecd107f1c62179b65dcacaccd7f
chochoq <aubepe@gmail.com>
2021-02-25 15:07:15 +0900

    (feat)update init_db
    다음
    날씨, 날짜
    db저장 (근데 이거 매일 달라지는 건데 잉...잉....잉..?)

--- python/SerchYoutube/init_db.py
+++ python/SerchYoutube/init_db.py
@@ -3,6 +3,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -17,21 +21,19 @@ for li in lis:
     weatherIcon = li.select_one('strong').text
     weatherDo = li.select_one('em').text
     weatherDoC = li.select_one('span.img_weather.ico_degree > span').text
-    print(weatherArea,weatherIcon,weatherDo,weatherDoC)
+
+    doc = {
+        'weatherArea':weatherArea,
+        'weatherIcon':weatherIcon,
+        'weatherDo':weatherDo,
+        'weatherDoC':weatherDoC
+    }
+
+    db.daumWeathers.insert_one(doc)
 
 div = soup.select_one('#daumHead > div.d_service > div > div.today_base > div > div.info_today > strong').text
-print(div)
+dateDoc={
+    'div':div
+}
 
-
-# 날씨
-# 지역
-#minitoday > ul > li:nth-child(1) > span.txt_part
-# 현재날씨아이콘으로(가져올수있나..)
-#minitoday > ul > li:nth-child(1) > strong
-# 몇도
-#minitoday > ul > li:nth-child(1) > em
-# 뒤에 도
-#minitoday > ul > li:nth-child(19) > span.img_weather.ico_degree > span
-
-# 날짜
-#daumHead > div.d_service > div > div.today_base.today_on > div > div.info_today > strong
+db.daumDate.insert_one(dateDoc)
