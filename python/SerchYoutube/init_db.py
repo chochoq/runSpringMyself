81a1fa3eb6f550920c069ba486a1c34492b90b4a
chochoq <aubepe@gmail.com>
2021-02-25 14:48:02 +0900

    (feat)update init_db
    다음
    날씨, 날짜받아옴
    
    이걸바로 html에 출력하고싶은데 db에 넣어서 하는 법밖에 모르므로 우선 그렇게 해야할것같다

--- python/SerchYoutube/init_db.py
+++ python/SerchYoutube/init_db.py
@@ -10,14 +10,28 @@ data = requests.get('https://www.daum.net/', headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
 
-print(soup)
+lis = soup.select('#minitoday > ul > li')
+
+for li in lis:
+    weatherArea = li.select_one('span.txt_part').text
+    weatherIcon = li.select_one('strong').text
+    weatherDo = li.select_one('em').text
+    weatherDoC = li.select_one('span.img_weather.ico_degree > span').text
+    print(weatherArea,weatherIcon,weatherDo,weatherDoC)
+
+div = soup.select_one('#daumHead > div.d_service > div > div.today_base > div > div.info_today > strong').text
+print(div)
+
 
 # 날씨
-#daumHead > div.d_service > div > div.today_base > div
-#minitoday > ul > li:nth-child(1)
-#minitoday > ul > li:nth-child(2)
+# 지역
+#minitoday > ul > li:nth-child(1) > span.txt_part
+# 현재날씨아이콘으로(가져올수있나..)
+#minitoday > ul > li:nth-child(1) > strong
+# 몇도
+#minitoday > ul > li:nth-child(1) > em
+# 뒤에 도
+#minitoday > ul > li:nth-child(19) > span.img_weather.ico_degree > span
 
 # 날짜
-#daumHead > div.d_service > div > div.today_base > div
-#minitoday
-#daumHead > div.d_service > div > div.today_base > div > div.info_today > strong
+#daumHead > div.d_service > div > div.today_base.today_on > div > div.info_today > strong
