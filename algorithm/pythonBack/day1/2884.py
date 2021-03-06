8378bb43b292b945a4e38f9b57d147059ff38831
chochoq <aubepe@gmail.com>
2021-03-06 18:49:33 +0900

    add 4344

--- algorithm/pythonBack/day1/2884.py
+++ algorithm/pythonBack/day1/2884.py
@@ -17,3 +17,21 @@ else :
     M = 60 + time
     
 print(H,M)
+
+
+# 변수를 입력받는 방법
+H,M = input().split()
+#문자를 숫자로
+H = int(H)
+M = int(M)
+# 풀이
+if M < 45 and M >= 0:
+    if H < 24 and H > 0:
+        H = H - int(1)
+        M = int(60) + (M - int(45))
+    elif H == int(0):
+        H = 23
+        M = int(60) + (M - int(45))        
+elif M >= 45 and M < 60:
+    M = M - int(45)
+print(H,M)
