3da935ca9d8c8ff785dc59e2b0381dd7263e5d52
chochoq <aubepe@gmail.com>
2021-03-08 22:00:09 +0900

    edit 1929

--- algorithm/pythonBack/day3/1929.py
+++ algorithm/pythonBack/day3/1929.py
@@ -22,15 +22,15 @@
 
 import math
 
-# m, n = list(map(int, input().split()))
-# s = [0, 0] + [1] * (n - 1)
-# for i in range(2, int(n ** .5) + 1):
-#     # 에라토스테네스의 체 
-#     if s[i]:
-#         s[i * 2::i] = [0] * ((n - i) // i)
-# for i, v in enumerate(s):
-#     if v and i >= m:
-#         print(i)
+m, n = list(map(int, input().split()))
+s = [0, 0] + [1] * (n - 1)
+for i in range(2, int(n ** .5) + 1):
+    # 에라토스테네스의 체 
+    if s[i]:
+        s[i * 2::i] = [0] * ((n - i) // i)
+for i, v in enumerate(s):
+    if v and i >= m:
+        print(i)
 
 # - 먼저 m, n을 입력으로 받고, 
 # 0부터 n까지 숫자에 대한 리스트를 만드는데, 
@@ -47,19 +47,23 @@ import math
 # - 이렇게 해서 소수번째만 1을 가지고 있는 리스트가 완성됨. 
 # m 이상의 수에 대해서, 값이 1인 위치만 출력해주면 됨.
 
+
 def isPrime(i) :
     if i != 1:
+        # 1은 소수가 아님
         j = int(math.sqrt(i)) 
-        #제곱근인것은 약수! 
+        # 제곱근인것은 약수! 
         # i의 제곱근, 제곱근으로 할시 소수점이기때문에 int로 소수점을 제외시킴
         for x in range(2, j+1):
             # 소수의 정의가 1을 제외하기 때문에 2부터 시작해서 뒤의 j+1은 미만이기때문에 
-            if i % x == 0: 
-            # 2와 J+1사이의 값들  나머지가 0일경우 약수기때문에 false
+            if i % x == 0: # -> 나머지가 0이면 약수 
+            # 2와 J+1사이의 값들  
+            # 나머지가 0일경우 약수기 때문에 false 
                 return False
         return True
 
 M, N = map(int, input().split())
+# map 요소를 받음
 
 for k  in range(M, N+1):
     if isPrime(k) : 
