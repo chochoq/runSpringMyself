ac7709e3656a5c720f838f0fa47a928b69f079f4
chochoq <aubepe@gmail.com>
2021-03-11 20:35:18 +0900

    add *

---
+++ algorithm/pythonBack/basicEx/0311.py
@@ -0,0 +1,20 @@
+# 재귀함수
+# 1부터 N까지의 합과 곱
+
+# x = 0
+# for i in range(1, 101):
+#     x += i
+# print(x)
+
+# 곱하기
+# x = 1
+# for i in range(1, 6):
+#     x *= i
+# print(x)
+
+
+# 시그마 공식 n*(n+1)//2
+# n = 100
+# x = 1
+# x = n*(n+1)//2
+# print(x)
